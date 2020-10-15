f0e67c4fbd936fe21b9c23e44f8f60bbc1c82683
John Doe <johndoe@hp.com>
2020-10-15 22:42:32 +0800

    1. create project, add plugins, common code

---
+++ lib/widgets/common_text_style.dart
@@ -0,0 +1,7 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+
+final commonTextStyle = TextStyle(fontSize: 16, color: Colors.black87);
+final smallCommonTextStyle = TextStyle(fontSize: 12, color: Colors.black87);
+final smallGrayTextStyle = TextStyle(fontSize: 12, color: Colors.grey);
